a5c15467ac552d7982b6058d80b7f6efa6638f61
Rahul Pawar <rahulpawar826976@gmail.com>
2026-08-21 10:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (46.55%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -5,7 +5,7 @@ public:
         while(start<=end)
         {
 
-            mid = (start+end)/2;
+            mid = start + (end-start)/2;
             if(nums[mid]==target)
             {
                 return mid;
